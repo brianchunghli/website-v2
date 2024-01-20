8cddd4a96491a999c0419b99222cf4b35337a654
Brian Li <brianchunghli@gmail.com>
2024-01-20 17:28:04 +1100

    ci: remove libre baskerville and update dependencies

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -6,10 +6,8 @@ export default function footer() {
     <>
       <footer id="footer-content">
         <small>
-          Built with Vite and React and deployed with Vercel. The typefaces used are
-          <a href="https://fontsource.org/fonts/inter" className="highlight"> Inter </a>
-          and <a href="https://fontsource.org/fonts/libre-baskerville" className="highlight">
-            Libre Baskerville</a>. Colours seen are a subset of the <a className="highlight" href="https://github.com/catppuccin/catppuccin">Catpuccin theme</a>.
+          Built with Vite and React and deployed with Vercel. The typeface used is
+          <a href="https://fontsource.org/fonts/inter" className="highlight"> Inter</a> and the colours seen are a subset of the <a className="highlight" href="https://github.com/catppuccin/catppuccin">Catpuccin theme</a>.
         </small>
       </footer>
     </>
