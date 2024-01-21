69fcc711e5e4fdfbc252e22e28aa99d47d41ee15
Brian Li <brianchunghli@gmail.com>
2024-01-21 19:54:37 +1100

    refactor: updated skills list rendering conditions

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -5,15 +5,20 @@ import SkillList from "../components/SkillList";
 import FancySection from "../components/fancySection";
 import { ImageContentEntry, ComponentDelay } from "../types";
 
-
-//const contents: ContentEntry[] = [{ name: "Hi", desc: "Okay", skills: ["okay"] }];
 const imageContents: ImageContentEntry[] = [
   {
     link: "https://brianli-personal-website.vercel.app",
-    name: "Personal website v1",
+    title: "Splitfast",
+    desc: "A splitwise clone built using the Rust Axum framework and React with state persistence, authentication and sharing. Backend deployed via an AWS EC2 instance and Docker.",
+    image: "/website-v1.png",
+    skills: ["Rust", "Typescript", "Axum", "React", "Docker", "Amazon Web Services"]
+  },
+  {
+    link: "https://brianli-personal-website.vercel.app",
+    title: "Personal website v1",
     desc: "The first iteration of my personal website.",
     image: "/website-v1.png",
-    skills: ["Typescript", "Vite", "CSS"]
+    skills: ["Typescript", "React", "Vite", "Css"]
   }
 ];
 
@@ -26,11 +31,13 @@ export default function ProjectsSection({ id, mainDelay = 1000, ...props }: Proj
       <FadeLeftRight delay={mainDelay} FadeLeft={false}>
         <span id="project-section-title" className="title"><b>PROJECTS</b></span>
         <ul id="project-section-content">
-          {imageContents.map(({ link, name, desc, image, skills }, i) =>
-            <li key={`${name}-${i}`} >
+          {imageContents.map(({ link, title, desc, image, skills }, i) =>
+            <li key={`${title}-${i}`} >
               <ImageCard imageUrl={image} href={link} >
-                <FancySection name={name} desc={desc} />
-                <SkillList name={name} items={skills} />
+                <FancySection name={title} desc={desc} link={link} />
+                {skills &&
+                  <SkillList name={title} items={skills} />
+                }
               </ImageCard>
             </li>)}
         </ul>
