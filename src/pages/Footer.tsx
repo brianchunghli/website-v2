b272f28a4763e2cccbb0a4f365390ab9e7d58163
Brian Li <brianchunghli@gmail.com>
2024-01-21 19:47:36 +1100

    refactor: updated footer message and styling

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,5 +1,4 @@
 import '../styles/Footer.css'
-import FadeLeftRight from "../components/FadeLeftRight";
 
 export default function footer() {
   return (
@@ -7,7 +6,7 @@ export default function footer() {
       <footer id="footer-content">
         <small>
           Built with Vite and React and deployed with Vercel. The typeface used is
-          <a href="https://fontsource.org/fonts/inter" className="highlight"> Inter</a> and the colours seen are a subset of the <a className="highlight" href="https://github.com/catppuccin/catppuccin">Catpuccin theme</a>.
+          <a href="https://fontsource.org/fonts/inter" className="highlight"> Inter</a>, and the colours are drawn from a subset of the <a className="highlight" href="https://github.com/catppuccin/catppuccin">Catpuccin theme</a>.
         </small>
       </footer>
     </>
