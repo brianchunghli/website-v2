a2b2b3d78d494a3572efa3cd68a2f0092ee375b1
Brian Li <brianchunghli@gmail.com>
2024-01-28 09:57:25 +1100

    chore: fix up about me section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -14,14 +14,14 @@ export default function AboutSection({ mainDelay, ...props }: AboutSectionProps)
       <FadeLeftRight delay={mainDelay} FadeLeft={false}>
         <div id="about-section-content">
           <div>
-            My first experience with web development happened back in 2012 while customizing my Myspace page—a small but meaningful beginning in my journey into software development. Over time, my interest in building practical solutions has only grown.
+            My first experience with web development happened back in 2012 while customizing my Myspace page. Back then — it felt like a big step at the time, in hindsight, it was the first small step to my exciting adventure in software development.
           </div>
           <div>
-            After a brief period at PwC Consulting, I realized my true passion lies in creating effective solutions to everyday challenges. So, I made the decision to leave PwC and focus on what I love.
+            After a brief period at PwC Consulting, I realized my true passion lies in creating effective solutions to both everyday and novel challenges. So, I made the decision to leave PwC and focus on what I enjoyed doing the most.
+            I'm all about that sweet spot where design and engineering meet. It's not just about making things work; it's about making them look and feel good as well.
           </div>
-
           <div>
-            For now, stay tuned for updates on my current projects.
+            My experience and the things that I've tinkered with or have worked on to date can be found below. When I'm not coding, you'll find me deep in some theoretical computer science rabbit hole, playing badminton, or reading.
           </div>
         </div>
       </FadeLeftRight>
