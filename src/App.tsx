57ace5a6fd31d95ce6f08bd76f5f91ef7343a78d
Brian Li <brianchunghli@gmail.com>
2024-01-31 16:12:13 +1100

    build: revert analytics and inject via html instead

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import './App.css'
-import { Analytics } from '@vercel/analytics/react'
 
 function App() {
   return <>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Analytics/>
     </Routes>
   </>
 }
